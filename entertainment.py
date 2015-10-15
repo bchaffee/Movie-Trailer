76853830d83bd91841e835717b9bb4f553142b1f
bchaffee <brandon.c.chaffee@gmail.com>
2015-10-14 18:43:50 -0700

    Submission 4
    Modified line length and indentation

--- entertainment.py
+++ entertainment.py
@@ -1,36 +1,32 @@
+import fresh_tomatoes
+import media
+
 __author__ = "Brandon Chaffee"
 __date__ = "10/11/2015"
 
 # entertainment.py creates instances of media.Movie
 
-
-import fresh_tomatoes
-import media
-
-#each of the following is an instance of media.Movie
+# each of the following is an instance of media.Movie
 interstellar = media.Movie(
     "Interstellar",
     "A team of explorers travel through a wormhole in space\
     in an attempt to ensure humanity's survival.",
     "https://www.youtube.com/watch?v=0vxOhd4qlnA",
-    "http://ia.media-imdb.com/images/M/MV5BMjIxNTU4MzY4M\
-    F5BMl5BanBnXkFtZTgwMzM4ODI3MjE@._V1_SX640_SY720_.jpg")
+    "http://ia.media-imdb.com/images/M/MV5BMjIxNTU4MzY4MF5BMl5BanBnXkFtZTgwMzM4ODI3MjE@._V1_SX640_SY720_.jpg")  # noqa
 
 forrest_gump = media.Movie(
     "Forrest Gump",
     "Forrest Gump, while not intelligent, has accidentally been present\
     at many historic moments, but his true love, Jenny Curran, eludes him.",
     "https://www.youtube.com/watch?v=uPIEn0M8su0",
-    "https://1001scribbles.files.wordpress.com/2012/04/forrest\
-    -gump-poster1.jpg")
+    "https://1001scribbles.files.wordpress.com/2012/04/forrest-gump-poster1.jpg")  # noqa
 
 finding_nemo = media.Movie(
     "Finding Nemo",
     "After his son is captured in the Great Barrier Reef and taken\
     to Sydney, a timid clownfish sets out on a journey to bring him home.",
     "https://www.youtube.com/watch?v=wZdpNglLbt8",
-    "http://img3.wikia.nocookie.net/__cb20150108180911/disney/images\
-    /8/89/Finding_Nemo_-_Poster.jpg")
+    "http://img3.wikia.nocookie.net/__cb20150108180911/disney/images/8/89/Finding_Nemo_-_Poster.jpg")  # noqa
 
 spirited_away = media.Movie(
     "Spirited Away",
@@ -38,8 +34,7 @@ spirited_away = media.Movie(
     girl wanders into a world ruled by gods, witches, and spirits,\
     and where humans are changed into beasts.",
     "https://www.youtube.com/watch?v=ByXuk9QqQkk",
-    "http://ia.media-imdb.com/images/M/MV5BMjYxMDcyMzIzNl5BMl5BanBn\
-    XkFtZTYwNDg2MDU3._V1_SX640_SY720_.jpg")
+    "http://ia.media-imdb.com/images/M/MV5BMjYxMDcyMzIzNl5BMl5BanBnXkFtZTYwNDg2MDU3._V1_SX640_SY720_.jpg")  # noqa
 
 braveheart = media.Movie(
     "Braveheart",
@@ -55,8 +50,7 @@ inception = media.Movie(
     technology is given the inverse task of planting an idea into the\
     mind of a CEO.",
     "https://www.youtube.com/watch?v=66TuSJo4dZM",
-    "http://ia.media-imdb.com/images/M/MV5BMjAxMzY3NjcxNF5BMl5BanBnXkFt\
-    ZTcwNTI5OTM0Mw@@._V1_SX640_SY720_.jpg")
+    "http://ia.media-imdb.com/images/M/MV5BMjAxMzY3NjcxNF5BMl5BanBnXkFtZTcwNTI5OTM0Mw@@._V1_SX640_SY720_.jpg")  # noqa
 
 # creates array of each Movie instance
 movies = [
--- media.py
+++ media.py
@@ -1,23 +1,28 @@
+import webbrowser
+
 __author__ = "Brandon Chaffee"
 __date__ = "10/11/2015"
 
-import webbrowser
 
 class Movie():
         """...class Movie docstring...
-        this class is used to initialized and show the trailer of each instance of the class Movie in the webbrowser
+        this class is used to initialized and show the trailer of each instance
+        of the class Movie in the webbrowser
         """
-	def __init__(self, movie_title, movie_storyline, trailer_youtube, poster_image):
+        def __init__(self, movie_title, movie_storyline, trailer_youtube,
+                     poster_image):
                 """...constructor docstring...
-                the constructor creates instance variables for each of the parameters from the Movie class instances
+                the constructor creates instance variables for each of the
+                parameters from the Movie class instances
                 """
-		self.title = movie_title
-		self.storyline = movie_storyline
-		self.trailer_youtube_url = trailer_youtube
-		self.poster_image_url = poster_image
+                self.title = movie_title
+                self.storyline = movie_storyline
+                self.trailer_youtube_url = trailer_youtube
+                self.poster_image_url = poster_image
 
-	def show_trailer(self):
+        def show_trailer(self):
                 """...show_trailer docstring...
-                show_trailer method takes in the trailer instance variable and opens the corresponding video in the web browser
-                """ 
-		webbrowser.open(self.trailer) 
+                show_trailer method takes in the trailer instance variable and
+                opens the corresponding video in the web browser
+                """
+                webbrowser.open(self.trailer)
